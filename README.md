17e46b596c3bf2ce00c24bb06baf8c7d025570ac
Ben Ogle <ogle.ben@gmail.com>
2018-08-31 11:25:42 -0700

    Initial

---
+++ README.md
@@ -0,0 +1,82 @@
+# Anvil Encryption
+
+This library is a small wrapper around node's `crypto` library. It offers convenience methods for encrypting and decrypting arbitrary payloads in both AES and RSA.
+
+## Install
+
+```sh
+yarn add @anvilco/encryption
+```
+
+Then use it
+
+```js
+// For RSA functions
+import { encryptRSA, decryptRSA } from '@anvilco/encryption'
+
+// For AES functions
+import { generateAESKey, encryptAES, decryptAES } from '@anvilco/encryption'
+
+
+// RSA
+const message = 'Super secret message'
+const encryptedRSAMessage = encryptRSA('...public key....', message)
+const origRSAMessage = decryptRSA('...private key....', encryptedRSAMessage)
+console.log(origRSAMessage === message) // => true
+
+// AES
+const aesKey = generateAESKey()
+const encryptedAESMessage = encryptAES(aesKey, message)
+const origAESMessage = decryptAES(aesKey, encryptedAESMessage)
+console.log(origAESMessage === message) // => true
+```
+
+## API
+
+There are two functions for RSA: `encryptRSA`, `decryptRSA`, and three for AES: `generateAESKey`, `encryptAES`, `decryptAES`.
+
+### AES
+
+There are three functions for AES:
+
+* `key = generateAESKey()`
+* `encryptedMessage = encryptAES(key, plainMessage)`
+* `plainMessage = decryptAES(key, encryptedMessage)`
+
+`encryptAES` creates a unique [IV](https://en.wikipedia.org/wiki/Initialization_vector) for each message encrypted and prepends it to the resulting encrypted payload. So `encryptedMessage` will be in the format
+
+```
+<iv>:<aesEncryptedPayload>
+```
+
+### RSA
+
+There are two functions for RSA:
+
+* `encryptedMessage = encryptRSA(publicKey, plainMessage)`
+* `plainMessage = decryptRSA(privateKey, encryptedMessage)`
+
+These functions encrypt / decrypt with RSA _and_ AES. RSA has an upper limit on how much data it can encrypt. So we create an AES key, encrypt the AES key with RSA, then encrypt the actual message with AES.
+
+Note the encrypted payload (result of `encryptRSA`) is in the format:
+
+```
+<rsaEncryptedAESKey>:<aesEncryptedMessage>
+```
+
+#### Generating RSA keys
+
+This library does not have a way to generate RSA keys. You can use openssl or something like [node-rsa](https://github.com/rzcoder/node-rsa):
+
+```js
+// Example generating a RSA keypair with node-rsa
+import NodeRSA from 'node-rsa'
+function generateRSAKeypair () {
+  const key = new NodeRSA()
+  key.generateKeyPair(2048, 65537)
+  return {
+    publicKey: key.exportKey('pkcs8-public-pem'),
+    privateKey: key.exportKey('pkcs8-private-pem'),
+  }
+}
+```
---
+++ index.js
@@ -0,0 +1,89 @@
+// See
+// https://gist.github.com/vlucas/2bd40f62d20c1d49237a109d491974eb
+// http://vancelucas.com/blog/stronger-encryption-and-decryption-in-node-js/
+
+import crypto from 'crypto'
+
+export const IVLength = 16 // For AES, this is always 16
+export const AESKeyLength = 32 // Must be 256 bits (32 characters)
+
+//
+// RSA
+//
+
+// Public: Encrypt with RSA algorithm
+//
+// RSA has an upper limit on how much data it can encrypt. So we create an AES
+// key, encrypt the AES key with RSA, then encrypt the actual message with AES.
+//
+// * `publicKey` {String} RSA public key
+// * `message` {String} Message to encrypt
+//
+// Returns a {String} like 'abcdef:abcdef:abcdef'
+//   which is '<rsaEncryptedAESKey:aesIV:aesEncryptedMessage>'
+export function encryptRSA (publicKey, message) {
+  const aesKey = generateAESKey()
+  const enc = crypto.publicEncrypt({
+    key: publicKey,
+    padding: crypto.RSA_PKCS1_OAEP_PADDING,
+  }, Buffer.from(aesKey))
+  const encAesKey = enc.toString('base64')
+  return `${encAesKey}:${encryptAES(message, aesKey)}`
+}
+
+// Public: Decrypt with RSA algorithm
+//
+// * `privateKey` {String} RSA public key
+// * `message` {String} Encrypted message to decrypt
+//
+// Returns {String} decrypted message
+export function decryptRSA (privateKey, message) {
+  const index = message.indexOf(':')
+  const encAesKey = message.slice(0, index)
+  const encryptedMessage = message.slice(index + 1)
+  const enc = crypto.privateDecrypt({
+    key: privateKey,
+    padding: crypto.RSA_PKCS1_OAEP_PADDING,
+  }, Buffer.from(encAesKey, 'base64'))
+  const aesKey = enc.toString()
+  return decryptAES(encryptedMessage, aesKey)
+}
+
+//
+// AES
+//
+
+// Public: Returns a {String} random AES key
+export function generateAESKey () {
+  return Buffer.from(crypto.randomBytes(32)).toString('hex')
+}
+
+// Public: Encrypts with AES
+//
+// * `key` {String} AES key
+// * `message` {String} Message to encrypt
+//
+// Returns {String} encrypted message
+export function encryptAES (key, message) {
+  const iv = crypto.randomBytes(IVLength)
+  const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key, 'hex'), iv)
+  let encrypted = cipher.update(message)
+  encrypted = Buffer.concat([encrypted, cipher.final()])
+  return iv.toString('hex') + ':' + encrypted.toString('hex')
+}
+
+// Public: Decrypts with AES
+//
+// * `key` {String} AES key
+// * `message` {String} Encrypted message to decrypt
+//
+// Returns {String} decrypted message
+export function decryptAES (key, message) {
+  const textParts = message.split(':')
+  const iv = Buffer.from(textParts.shift(), 'hex')
+  const encryptedText = Buffer.from(textParts.join(':'), 'hex')
+  const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key, 'hex'), iv)
+  let decrypted = decipher.update(encryptedText)
+  decrypted = Buffer.concat([decrypted, decipher.final()])
+  return decrypted.toString()
+}
